a1ee9936c11ef210370e59e4fe65abca8bcdb63d
Franz <franz.kunze@tum.de>
2025-07-15 12:00:28 +0200

    Init

--- backend/app/workflow/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Workflow package exposing supervisor processing utilities."""
-from .supervisor import process_claim_with_supervisor, create_insurance_supervisor  # noqa: F401
-
-__all__ = [
-    "process_claim_with_supervisor",
-    "create_insurance_supervisor",
-]
